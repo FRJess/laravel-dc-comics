7cd46dca7d1eb3d81c568aca0f1912dcfe91dee9
FR_Jess <fr_jess@icloud.com>
2023-01-12 16:38:35 +0100

    [add] table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1>Comics list</h1>
+  <div class="container">
+    <h1>Comics list</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Series</th>
+          <th scope="col">Type</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @forelse ($comics as $comic)
+          <tr>
+            <td>{{ $comic->id }}</td>
+            <td>{{ $comic->title }}</td>
+            <td>{{ $comic->series }}</td>
+            <td>{{ $comic->type }}</td>
+            <td>Modify</td>
+          </tr>
+        @empty
+          <h1>No product found</h1>
+        @endforelse
+      </tbody>
+    </table>
+
+    {{ $comics->links() }}
+
+  </div>
 @endsection
