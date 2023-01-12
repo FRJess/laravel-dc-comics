210bd774578fe275c7cb1e6bbaac3b8a9d573a1e
FR_Jess <fr_jess@icloud.com>
2023-01-12 15:09:41 +0100

    [add] slug in table

--- database/migrations/2023_01_12_140105_create_comics_table.php
+++ database/migrations/2023_01_12_140105_create_comics_table.php
@@ -15,12 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->nullable();
+            $table->slug('slug', 110)->unique();
             $table->string('thumb');
-            $table->string('title', 100);
             $table->text('description');
             $table->string('series', 50);
-            $table->string('price', 10);
-            $table->string('sale_date', 15);
+            $table->string('price', 10)->nullable();
+            $table->string('sale_date', 15)->nullable();
             $table->string('type', 20);
             $table->timestamps();
         });
