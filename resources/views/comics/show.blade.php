2cc3921b69ad17a32ce333f397f2a7d2d0f6a369
FR_Jess <fr_jess@icloud.com>
2023-01-12 16:56:11 +0100

    [add] details page

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+  <div class="container">
+    <h1>{{ $comic->title }}</h1>
+    <div>Series: <strong>{{ $comic->series }}</strong></div>
+    <div>Type: <strong>{{ $comic->type }}</strong></div>
+    <div>Release date: <strong>{{ $comic->sale_date }}</strong></div>
+    <div>Price: <strong>${{ $comic->price }}</strong></div>
+    <div class="text-center my-4">
+      <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+    </div>
+    <p>{!! $comic->description !!}</p>
+
+  </div>
+@endsection
