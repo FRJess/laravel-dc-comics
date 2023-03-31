8008883213e9a25dd739a1aa79e08cd627d795eb
Jessica <fr_jess@icloud.com>
2023-03-31 11:41:45 +0200

    [fix] view

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-  <div class="container">
-    <h1>{{ $comic->title }}</h1>
-    <div>Series: <strong>{{ $comic->series }}</strong></div>
-    <div>Type: <strong>{{ $comic->type }}</strong></div>
-    <div>Release date: <strong>{{ $comic->sale_date }}</strong></div>
-    <div>Price: <strong>${{ $comic->price }}</strong></div>
-    <div class="text-center my-4">
-      <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-    </div>
-    <p>{!! $comic->description !!}</p>
-    <a class="btn btn-primary" href="{{ route('comics.index') }}">Go back to list</a>
+ <div class="container p-4">
+  <h1>{{ $comic->title }}</h1>
+  <div>Series: <strong>{{ $comic->series }}</strong></div>
+  <div>Type: <strong>{{ $comic->type }}</strong></div>
+  <div>Release date: <strong>{{ $comic->sale_date }}</strong></div>
+  <div>Price: <strong>{{ $comic->price }}</strong></div>
+  <div class="text-center my-4">
+   <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
   </div>
+  <p>{!! $comic->description !!}</p>
+  <a class="btn btn-primary" href="{{ route('comics.index') }}">Go back to list</a>
+ </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,31 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>DC Comics</title>
+ <title>DC Comics</title>
 
-  <!-- Fonts -->
-  <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+ <!-- Fonts -->
+ <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-  <!-- Font Awesome -->
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css'
-    integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ=='
-    crossorigin='anonymous' />
+ <!-- Font Awesome -->
+ <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css'
+  integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ=='
+  crossorigin='anonymous' />
 
-  <!-- Styles -->
-  @vite('resources/js/app.js')
+ <!-- Styles -->
+ @vite('resources/js/app.js')
 
 </head>
 
 <body class="bg-dark text-white">
 
-  @include('partials.header')
+ @include('partials.header')
 
-  @yield('content')
+ @yield('content')
 
-  @include('partials.footer')
+ @include('partials.footer')
 
 </body>
 
